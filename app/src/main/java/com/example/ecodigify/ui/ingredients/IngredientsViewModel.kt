c552c11b14d61d55704d52e20c14698da2f997e7
LucaBarban <45258881+LucaBarban@users.noreply.github.com>
2024-11-23 17:59:20 +0100

    Renaming Template's things

--- app/src/main/java/com/example/ecodigify/ui/ingredients/IngredientsViewModel.kt
+++ app/src/main/java/com/example/ecodigify/ui/ingredients/IngredientsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.ecodigify.ui.home
+package com.example.ecodigify.ui.ingredients
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class IngredientsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is Ingredients Fragment"
     }
     val text: LiveData<String> = _text
 }
