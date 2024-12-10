e91c4c8dfc3c8cf7d0c86ec24035487c5840f649
Luca Saccarola <github.e41mv@aleeas.com>
2024-12-10 17:38:26 +0100

    Clean unused imports

--- app/src/main/java/com/example/ecodigify/ui/popup/PopupIngredientsActivity.kt
+++ app/src/main/java/com/example/ecodigify/ui/popup/PopupIngredientsActivity.kt
@@ -12,7 +12,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.NumberPicker
 import android.widget.Spinner
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
